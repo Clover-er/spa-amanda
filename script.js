84e331a9783d98d2074541918b4063e147bad3a7
clover-er <aki.amanda99@gmail.com>
2025-04-15 21:01:02 -0400

    realicé la actividad

--- script.js
+++ script.js
@@ -8,17 +8,17 @@ function render() {
   switch (route) {
     case "home":
       content.innerHTML = `
-        <h1>🌐 Sitio en Construcción</h1>
-        <p>Bienvenido al proyecto. Aún estamos trabajando en los contenidos.</p>
+        <h1>🌐 Construyendo...</h1>
+        <p>bienveDINOS 🦖 al proyecto. <br>Aún estamos trabajando en la era mesozoica.</p>
       `;
       break;
     case "carrusel":
       content.innerHTML = `
         <h2>🖼️ Carrusel de Imágenes</h2>
         <div class="carrusel">
-          <img src="https://via.placeholder.com/500x200?text=Imagen+1" class="active" />
-          <img src="https://via.placeholder.com/500x200?text=Imagen+2" />
-          <img src="https://via.placeholder.com/500x200?text=Imagen+3" />
+          <img src="img1.jpg" class="active" />
+          <img src="img2.jpg" class="active"/>
+          <img src="img3.jpg" class="active"/>
         </div>
       `;
       iniciarCarrusel();
@@ -27,22 +27,56 @@ function render() {
       content.innerHTML = `
         <h2>📋 Estructura por completar</h2>
         <h3>🖼️ Acá ingrese una imagen:</h3>
-        <!-- imagen aquí -->
+        <img src="img3.jpg" alt="Imagen de ejemplo">
 
         <h3>📄 Aquí va una tabla:</h3>
-        <!-- tabla aquí -->
+        <table border="1" cellpadding="10" cellspacing="5" width=80% height="200" align="center bgcolor="#f2f2f2">
+  <tr>
+    <th>Tabla</th>
+    <th>Tabla</th>
+    <th>Tabla</th>
+  </tr>
+  <tr>
+    <td>XXX</td>
+    <td>XXX</td>
+    <td>XXX</td>
+  </tr>
+  <tr>
+    <td>XXX</td>
+    <td>XXX</td>
+    <td>XXX</td>
+  </tr>
+</table>
 
         <h3>📑 Acá va una lista ordenada:</h3>
-        <!-- lista ordenada aquí -->
+        <ol>
+  <li>ListaOrdenada</li>
+  <li>ListaOrdenada</li>
+  <li>ListaOrdenada</li>
+  <li>ListaOrdenada</li>
+  <li>ListaOrdenada</li>
+</ol>
 
         <h3>📑 Acá va una lista no ordenada:</h3>
-        <!-- lista no ordenada aquí -->
+        <ul>
+  <li>ListaNoOrdenada</li>
+  <li>ListaNoOrdenada</li>
+  <li>ListaNoOrdenada</li>
+</ul>
 
         <h3>📥 Aquí debe ir un formulario:</h3>
-        <!-- formulario aquí -->
+        <form>
+  <label for="formulario">NombreFormulario:</label><br>
+  <input type="text" id="formulario" name="Formulario" /><br><br>
+  
+  <label for="email">NombreCorreo:</label><br>
+  <input type="email" id="email" name="email" /><br><br>
+
+  
 
         <h3>🔘 Aquí agregue un botón:</h3>
-        <!-- botón aquí -->
+        <input type="submit" value="Botón" />
+</form>
       `;
       break;
     default:
